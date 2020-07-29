4a3b60cd0a73d08cf6e0fd7d4852cc4d5486b2fe
Aleksey Efimov <factorng@yandex.ru>
2020-07-29 19:01:32 +0300

    question class added

--- components/Question.js
+++ components/Question.js
@@ -1,19 +1,41 @@
+/**
+ * Класс для рендера вопросов в разметку
+ * @typedef {Object} Question
+ * @property {Object} data
+ */
+
 export class Question {
-    // constructor(data) {
-    //     this._question = data.question;
-    //     this._answers = data.answers;
-    // }
-    _getQuestionTemplate() {
-        return document
-        .querySelector('#question')
-        .content
-        .querySelector('.card')
-        .cloneNode(true);
-    }
-    
-    getQuestionElement() {
-        this._questionElement = this._getQuestionTemplate();
-        document.querySelector('.cards').append(this._questionElement);
-        this._questionElement.querySelector('.card__question').innerText = 'fuck';
-    }
+  constructor(data) {
+    this._question = data.question;
+    this._answers = Array.from(data.answers);
+    this._questionNumber = data.number;
+    this._questionTotal = data.total;
+  }
+  _getQuestionTemplate() {
+    return document
+      .querySelector('#question')
+      .content
+      .querySelector('.card')
+      .cloneNode(true);
+  }
+  _getAnswerTemplate() {
+    return document
+      .querySelector('#answer')
+      .content
+      .cloneNode(true);
+  }
+  getQuestionElement() {
+    this._questionElement = this._getQuestionTemplate();
+    document.querySelector('.cards').append(this._questionElement);
+    this._questionElement.querySelector('.card__question').innerText = this._question;
+    this._questionElement.querySelector('.card__question-title')
+      .innerText = `Вопрос ${this._questionNumber} из ${this._questionTotal}`;
+
+    this._answers.forEach(answer => {
+      this._answerElement = this._getAnswerTemplate();
+      this._answerElement.querySelector('.card__answer').innerText = answer;
+      document.querySelector('.card__answers').append(this._answerElement);
+
+    });
+  }
 }
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import {Question} from './components/Question.js';
 
-const question = new Question();
+const data = {
+  question: 'Who let the dogs out?',
+  answers: ['Ban', 'Danila', 'Misha', 'Vitalic'],
+  number: 3,
+  total: 6,
+};
+const question = new Question(data);
 question.getQuestionElement();
