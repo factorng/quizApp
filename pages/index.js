ba64e97eea596c34c993db84185b2c2377eea294
Aleksey Efimov <factorng@yandex.ru>
2020-07-30 21:05:44 +0300

    index.js added

--- pages/index.js
+++ pages/index.js
@@ -17,53 +17,35 @@ const api = new Api({
   url: 'https://opentdb.com'
 });
 
-const card = new Card(
-  cardsContainer,
-  () => {
-    api.getQuestion(10, 18, 'hard')
-      .then(res => {
-        countQuestions = 0;
+api.getQuestion(10, 18, 'hard')
+  .then(res => {
+
+    const card = new Card(
+      cardsContainer,
+      () => {
+        countQuestions = 1;
         document.querySelector('.main__greeting').remove();
         document.querySelector('.main__button').remove();
-        newQuestion(data);
+        newQuestion(res.results[0]);
       });
-  });
 
-const handleNextClick = () => {
-  if (countQuestions === 10) {
-    document.querySelector('.card').remove();
-    card.renderResults();
-  } else {
-    countQuestions += 1;
-    console.log(countQuestions)
-    document.querySelector('.card').remove();
-    newQuestion(datatwo);
-  }
-}
+    const handleNextClick = () => {
+      if (countQuestions === 5) {
+        document.querySelector('.card').remove();
+        card.renderResults();
+      } else {
+        countQuestions += 1;
+        document.querySelector('.card').remove();
+        newQuestion(res.results[countQuestions]);
+      }
+    }
 
-const newQuestion = (data) => {
-  const question = new Question(data);
-  question.renderCard();
-  const buttonNext = document.querySelector(buttonNextSelector);
-  buttonNext.addEventListener('click', handleNextClick);
-}
+    card.renderTitleMenu()
 
-const data = {
-  question: 'Who let the dogs out?',
-  answers: ['Ban', 'Danila', 'Misha', 'Vitalic'],
-  rightAnswer: 'Danila',
-  number: 3,
-  total: 6,
-};
-
-const datatwo = {
-  question: 'What does the fox say?',
-  answers: ['Wa-pa-pa-pa-pa-pa-pow!', 'Hatee-hatee-hatee-ho!', 'Joff-tchoff-tchoffo-tchoffo-tchoff!', 'Tchoff-tchoff-tchoffo-tchoffo-tchoff!'],
-  rightAnswer: 'Wa-pa-pa-pa-pa-pa-pow!',
-  number: 3,
-  total: 6,
-};
-
-//api.getQuestion(10, 18, 'hard').then(res=>console.log(res));
-//api.getCategories();
-card.renderTitleMenu()
+    function newQuestion(obj) {
+      const question = new Question(obj);
+      question.renderCard();
+      const buttonNext = document.querySelector(buttonNextSelector);
+      buttonNext.addEventListener('click', handleNextClick);
+    }
+  })
